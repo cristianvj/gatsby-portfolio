e6950c4494a3eb99e0ce19f8f08bc93de51dc506
Cristian Villota <cristiandavid.vj@gmail.com>
2021-04-20 22:19:07 -0500

    add jobs buttons

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,6 @@ module.exports = {
       options: {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
-        // contentTypes: [`jobs`, `projects`, `blogs`, ],
-        //singleTypes:[`about` ]
         contentTypes: [`jobs`, `projects`, `blogs`],
         singleTypes: [`about`]
       },
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,8 +1,9 @@
 import React from "react"
 import Title from "./Title"
-import { FaAngleDoubleRight } from "react-icons/fa"
+//import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
+//import {faDivide} from '@fortawesome/free-solid-svg-icons';
 
 const query = graphql`
   {
@@ -22,7 +23,27 @@ const query = graphql`
 `
 
 const Jobs = () => {
-  return <h2>jobs component</h2>
+
+  const data = useStaticQuery(query)
+  const { allStrapiJobs: {nodes: jobs}} = data
+  const [value, setValue] = React.useState(0)
+  const {company, position, date, desc} = jobs[2]
+
+  return(
+    <section className="section jobs">
+      <Title title='experience' />
+      <div className='jobs-center'>
+        {/** btn container */}
+        <div className='btn-container'>
+          {
+            jobs.map((item, index)=>{
+              return <button key={item.strapiId} className={`job-btn ${index === value && 'active-btn'}`}>{item.company}</button>
+            })
+          } 
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Jobs
